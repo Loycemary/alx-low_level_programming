fb75437788aad5f2fd11f687a59447a865471937
Loycemary <loycemary2012@gmail.com>
2022-06-09 17:19:08 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,20 +7,21 @@
  */
 
 int _isupper(int c)
-{
-
+{ 
 	char uppercase = 'A';
 	int isupper = 0;
 
 	for (; uppercase <= 'Z'; uppercase++)
-	{
-
-		if (c == uppercase)
+	{ 
+		if(c == uppercase)
 		{
 			isupper = 1;
 			break;
+
 		}
 
 	}
+
 	return (isupper);
+
 }
