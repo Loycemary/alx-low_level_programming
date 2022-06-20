73742978ef51eacff7085d7fe1fdb0c11050fedb
Loycemary <loycemary2012@gmail.com>
2022-06-20 13:29:34 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+void char *_memset(char *s, char b, unsigned int n);
+{
+	char b;
+	unsigned int n;
+	char *s;
+
+	for (char b = 0; b < n; b++)
+
+	printf("%d", char b);
+
+}
+
+int main(void)
+{	unsigned int n = 10;
+:	char b;
+
+	printf("char *_memset()\n");
+	return (0);
+}
