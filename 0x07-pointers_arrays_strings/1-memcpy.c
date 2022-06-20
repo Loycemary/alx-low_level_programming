b91e2b7068293c69be03a980e3e19e1e3ac3ae4a
Loycemary <loycemary2012@gmail.com>
2022-06-20 14:30:15 -0700

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy -
+ * Return: *dest
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < 10; i++)
+
+	{
+		dest[i] = src[i]
+	}
+
+	return (dest);
+}
