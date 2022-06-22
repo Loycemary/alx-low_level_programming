834c89ba233eca939ee6b7856e7c3d8b96c41f6e
Loycemary <loycemary2012@gmail.com>
2022-06-21 18:45:38 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _memset - ironnou
- * @s: pointer to char params
- * @b: data to change
- * @n: index
- * Return: *s
+ * _memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: data to copy
+ * @n: number of times to copy b
+ * Return: pointer to the memory area s
  */
 
 char *_memset(char *s, char b, unsigned int n)
