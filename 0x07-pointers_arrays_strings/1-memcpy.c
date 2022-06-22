12ff7eefadcd9c4da48d5d4085720f1d4a66f72b
Loycemary <loycemary2012@gmail.com>
2022-06-21 17:14:06 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _memcpy -
+ * _memcpy - irontnoux2
+ * @dest: pointer to char params
+ * @src: pointer to char params
+ * @n: size
  * Return: *dest
- *
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < 10; i++)
-
+	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i]
+		dest[i] = src[i];
 	}
-
 	return (dest);
 }
