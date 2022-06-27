1cf348ef82085877796d6ad59256e5b0f50a92c1
Loycemary <loycemary2012@gmail.com>
2022-06-27 10:18:51 -0700

    Task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that prints arrays of characters
+ * Return: void
+ *
+ */
+
+int main (void)
+{ 
+	char *A[];
+	
+	A = malloc(sizeof(cha) *4);
+	 
+	if(size = 0);
+	A[0] = 'I'
+	B[1] = 'C'
+	C[2] = 'E'
+	D[3] = '\0'
+
+	printf("%c\n", A);
+
+	return (0);
+}
