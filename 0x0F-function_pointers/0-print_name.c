2a9e8f24cbc895b544e4c761a4fe615d1b2fd00f
Loycemary <loycemary2012@gmail.com>
2022-07-06 14:55:37 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,12 @@
+#include"stdlib.h"
 #include "function_pointers.h"
-#include "stdlib.h"
 #include <stdio.h>
-
 /**
- * print_name_as_is - prints name
+ * print_name - prints a name
  * @name: pointer to char
  * @f: pointer to function that returns nothing
- * Return: Nothing
- */
-
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && *name != '\0' && f != NULL)
