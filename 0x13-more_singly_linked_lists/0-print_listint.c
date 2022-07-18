38b2bea24b9602fafc0db5e6a76007e9caf4d7b9
Loycemary <loycemary2012@gmail.com>
2022-07-18 16:01:08 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,42 +3,22 @@
 #include <stdlib.h>
 
 /**
- * const listint - A function that prints all the elements of a linked list
- * @h: pointer to const listint
+ * print_listint - A function that prints all the elements of a linked list
+ * @h: pointer to first node
  * Return: the number of nodes
  *
  */
 
 size_t print_listint(const listint_t *h)
-
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * 
- */
-typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
-
-int main(void)
-
-{ 
-	print_elements(head);
-	if (head == NULL)
-		printf("%d\n", NULL);
-	struct node *h = NULL;
-	h = head;
+	size_t Godswill = 0;
 
 	while (h != NULL)
-
-	{	print("%d\n", h->elements);
-
-		h = h->link;
+	{
+		printf("%d\n", h->n);
+		Godswill++;
+		h = h->next;
 	}
 
+	return (Godswill);
 }
