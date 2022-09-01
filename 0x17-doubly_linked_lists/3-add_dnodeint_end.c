acd567004764fa26c25848219aef7c8e7bcd6726
Loycemary <loycemary2012@gmail.com>
2022-09-01 10:53:02 -0700

    Task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -28,6 +28,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (new_node);
 	}
 
+	while (temp_node->next != NULL)
+	{
+		temp_node = temp_node->next;
+	}
+
 	temp_node->next = new_node;
 	new_node->prev = temp_node;
 
